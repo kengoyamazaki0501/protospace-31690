698e3aaddae47df03a8d74561b52cfdd86f75a0c
kengoyamazaki0501 <kengo19890501@gmail.com>
2020-11-10 17:15:53 +0900

    コメント投稿機能、その他

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update, :destroy, :new, :create]
-  before_action :move_to_index, except: [:index, :new, :show,]
+  before_action :move_to_index, only:[:edit, :update,]
   def index
     @prototypes = Prototype.includes(:user)
   end
@@ -20,6 +20,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
+      redirect_to root_path
+    end
   end
 
   def update
@@ -49,7 +52,7 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    if user_signed_in?
+    unless user_signed_in?
       redirect_to action: :index
     end
   end
